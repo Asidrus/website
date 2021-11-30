40ffb6ce5c05e0c74900d52c8e984af037f1c8ab
Asidrus <asidrus@mail.ru>
2021-11-30 18:05:05 -0500

    v 0.2

--- testersite/urls.py
+++ testersite/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('parser/', include("siteparser.urls")),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
