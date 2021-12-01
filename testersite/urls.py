756cdd77afef187618e4f0f44ab2e764036cd3e4
Asidrus <asidrus@mail.ru>
2021-12-01 11:11:12 +0300

    0.1

--- testersite/urls.py
+++ testersite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -22,4 +23,6 @@ urlpatterns = [
     path('parser/', include("siteparser.urls")),
     path('', views.index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('change-password/', auth_views.PasswordChangeView.as_view()),
+    path('login/', auth_views.LoginView.as_view())
 ]
--- testersite/views.py
+++ testersite/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate, login
 
 def index(request):
     return render(request, "index.html")
     # return HttpResponse("hello world")
 
 
-
+def auth(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+    else:
+        pass
 
 
