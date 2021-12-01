0800f644384349bbdcd649c4fe9c6d9fe9e657a9
Asidrus <asidrus@mail.ru>
2021-12-01 18:03:21 +0300

    0.1.1

--- apps/siteparser/admin.py
+++ apps/siteparser/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 from .models import Parser
 
-# admin.site.register(Parser)
+admin.site.register(Parser)
--- apps/siteparser/models.py
+++ apps/siteparser/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from django.contrib import admin
-# Create your models here.
+from django.conf import settings
+
+from django.contrib.auth.models import User
 
 
-# class Parser(models):
-#     # userID = models.ForeignKey(admin, on_delete=models.CASCADE)
-#     # fileName = models.CharField('имя файла', max_length=50)
-#     pass
+class Parser(models.Model):
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    fileName = models.CharField('имя файла', max_length=50)
+
 
 
